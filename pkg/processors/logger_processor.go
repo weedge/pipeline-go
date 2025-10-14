3bc4bb675730a93dc03ca1554755f4f41b8dcb8a
weedge <weege007@gmail.com>
2025-10-14 19:45:08 +0800

    frame logger processor add maxIdToLogs
    Signed-off-by: weedge <weege007@gmail.com>

--- pkg/processors/logger_processor.go
+++ pkg/processors/logger_processor.go
@@ -32,6 +32,7 @@ type FrameLoggerProcessor struct {
 	prefix            string
 	ignoredFrameTypes []reflect.Type
 	includeFrameTypes []reflect.Type
+	maxIdToLogs       []uint64
 }
 
 // NewFrameLoggerProcessor 创建一个新的 FrameLoggerProcessor 实例
@@ -40,6 +41,7 @@ func NewFrameLoggerProcessor(
 	prefix string,
 	ignoredFrameTypes []frames.Frame,
 	includeFrameTypes []frames.Frame,
+	maxIdToLogs []uint64,
 ) *FrameLoggerProcessor {
 	ignoredTypes := make([]reflect.Type, len(ignoredFrameTypes))
 	for i, frame := range ignoredFrameTypes {
@@ -56,27 +58,61 @@ func NewFrameLoggerProcessor(
 		prefix:            prefix,
 		ignoredFrameTypes: ignoredTypes,
 		includeFrameTypes: includeTypes,
+		maxIdToLogs:       maxIdToLogs,
 	}
+}
+
+// NewFrameLoggerProcessorWithMaxIdToLogs creates a new FrameLoggerProcessor with a maximum number of IDs to log
+func NewFrameLoggerProcessorWithMaxIdToLogs(maxIdToLogs []uint64) *FrameLoggerProcessor {
+	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", []frames.Frame{}, []frames.Frame{}, maxIdToLogs)
 }
 
 // NewDefaultFrameLoggerProcessor 创建一个带有默认设置的 FrameLoggerProcessor
 func NewDefaultFrameLoggerProcessor() *FrameLoggerProcessor {
-	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", []frames.Frame{}, []frames.Frame{})
+	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", []frames.Frame{}, []frames.Frame{}, []uint64{})
 }
 
 // NewDefaultFrameLoggerProcessorWithName 创建一个带有默认设置的 FrameLoggerProcessor
 func NewDefaultFrameLoggerProcessorWithName(name string) *FrameLoggerProcessor {
-	return NewFrameLoggerProcessor(name, "Frame", []frames.Frame{}, []frames.Frame{})
+	return NewFrameLoggerProcessor(name, "Frame", []frames.Frame{}, []frames.Frame{}, []uint64{})
 }
 
 // NewDefaultFrameLoggerProcessorWithIncludeFrame 创建一个包括include frames设置的 FrameLoggerProcessor
 func NewDefaultFrameLoggerProcessorWithIncludeFrame(includeFrames []frames.Frame) *FrameLoggerProcessor {
-	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", []frames.Frame{}, includeFrames)
+	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", []frames.Frame{}, includeFrames, []uint64{})
 }
 
 // NewDefaultFrameLoggerProcessorWithIngoreFrame 创建一个忽略ignore frames设置的 FrameLoggerProcessor
 func NewDefaultFrameLoggerProcessorWithIngoreFrame(ingoreFrames []frames.Frame) *FrameLoggerProcessor {
-	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", ingoreFrames, []frames.Frame{})
+	return NewFrameLoggerProcessor("FrameLoggerProcessor", "Frame", ingoreFrames, []frames.Frame{}, []uint64{})
+}
+
+func (p *FrameLoggerProcessor) WithMaxIdToLogs(maxIdToLogs []uint64) *FrameLoggerProcessor {
+	p.maxIdToLogs = maxIdToLogs
+	return p
+}
+
+func (p *FrameLoggerProcessor) WithIncludeFrame(includeFrameTypes []frames.Frame) *FrameLoggerProcessor {
+	includeTypes := make([]reflect.Type, len(includeFrameTypes))
+	for i, frame := range includeFrameTypes {
+		includeTypes[i] = reflect.TypeOf(frame)
+	}
+	p.includeFrameTypes = includeTypes
+	return p
+}
+
+func (p *FrameLoggerProcessor) WithIgnoreFrame(ignoreFrameTypes []frames.Frame) *FrameLoggerProcessor {
+	ignoreTypes := make([]reflect.Type, len(ignoreFrameTypes))
+	for i, frame := range ignoreFrameTypes {
+		ignoreTypes[i] = reflect.TypeOf(frame)
+	}
+	p.ignoredFrameTypes = ignoreTypes
+	return p
+}
+
+func (p *FrameLoggerProcessor) WithPrefix(prefix string) *FrameLoggerProcessor {
+	p.prefix = prefix
+	return p
 }
 
 func (p *FrameLoggerProcessor) Prefix() string {
@@ -95,18 +131,24 @@ func (p *FrameLoggerProcessor) isIgnoredFrame(frame frames.Frame) bool {
 
 // isIncludedFrame 检查帧是否在包含列表中
 func (p *FrameLoggerProcessor) isIncludedFrame(frame frames.Frame) bool {
+	return p.indexIncludedFrame(frame) >= 0
+}
+
+// isIncludedFrame 检查帧是否在包含列表中
+func (p *FrameLoggerProcessor) indexIncludedFrame(frame frames.Frame) int {
 	if len(p.includeFrameTypes) == 0 {
-		return true
+		return -1
 	}
 
 	frameType := reflect.TypeOf(frame)
-	return slices.Contains(p.includeFrameTypes, frameType)
+	return slices.Index(p.includeFrameTypes, frameType)
 }
 
 func (p *FrameLoggerProcessor) ProcessFrame(frame frames.Frame, direction FrameDirection) {
 	// 检查是否应该处理此帧
 	if !p.isIgnoredFrame(frame) {
-		if p.isIncludedFrame(frame) {
+		index := p.indexIncludedFrame(frame)
+		if index >= 0 {
 			fromTo := p.name
 
 			// 只有当 prev 和 next 不为 nil 时才使用它们
@@ -125,7 +167,9 @@ func (p *FrameLoggerProcessor) ProcessFrame(frame frames.Frame, direction FrameD
 
 			msg := fmt.Sprintf("%s %s: (%T)%s", fromTo, p.prefix, frame, frame.String())
 
-			logger.Info(msg)
+			if len(p.maxIdToLogs) == 0 || (len(p.maxIdToLogs) > index && frame.ID()%p.maxIdToLogs[index] == 0) {
+				logger.Info(msg)
+			}
 		}
 	}
 
